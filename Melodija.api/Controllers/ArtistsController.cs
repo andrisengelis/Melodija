f4da6b2929b4b865652f83af589c543b6363fe10
Andris Engelis <andris.engelis@outlook.com>
2021-03-04 10:06:33 +0200

    Add Artists controller

---
+++ Melodija.api/Controllers/ArtistsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Melodija.api.Controllers
+{
+  [Route("api/artists")]
+  [ApiController]
+  public class ArtistsController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult GetArtists()
+    {
+      try
+      {
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        return StatusCode(500, "Internal server error");
+      }
+    }
+  }
+}
