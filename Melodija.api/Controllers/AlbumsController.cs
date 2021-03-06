17e0fab3b7c11a4930373bdf77e6a24f54293167
Andris Engelis <andris.engelis@outlook.com>
2021-03-06 20:52:49 +0200

    add albums controller

---
+++ Melodija.api/Controllers/AlbumsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Melodija.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Melodija.api.Controllers
+{
+  [Route("api/artists/{artistId}/albums")]
+  [ApiController]
+  public class AlbumsController : ControllerBase
+  {
+    private readonly IRepositoryManager _repository;
+    private readonly IMapper _mapper;
+
+    public AlbumsController(IRepositoryManager repository, IMapper mapper)
+    {
+      _repository = repository;
+      _mapper = mapper;
+    }
+    
+    
+  }
+}
