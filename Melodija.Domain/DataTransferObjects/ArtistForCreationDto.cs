4a57edfd87d4cf6dd698963b85458f92d679d6e6
Andris Engelis <andris.engelis@outlook.com>
2021-03-13 11:54:40 +0200

    Create Releases with artist

--- Melodija.Domain/DataTransferObjects/ArtistForCreationDto.cs
+++ Melodija.Domain/DataTransferObjects/ArtistForCreationDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Melodija.Domain.DataTransferObjects
+﻿using System.Collections.Generic;
+
+namespace Melodija.Domain.DataTransferObjects
 {
   public class ArtistForCreationDto
   {
     public string Name { get; set; }
     public string SortName { get; set; }
+    
+    public IEnumerable<ReleaseForCreationDto> Releases { get; set; }
   }
 }
